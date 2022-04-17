103f3d664ab91d83b7d73d1e25fd471c7d96a663
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-04-17 12:45:34 -0500

    add simple versions of files

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,25 @@
-#include "checkoutwindow.h"
+#include <QtCore/QFile>
+#include <QtCore/QStringLiteral>
+#include <QtCore/QUrl>
+#include <QtQml/QQmlApplicationEngine>
+#include <QtWidgets/QApplication>
 
-#include <QLoginDialog.h>
-#include <QAboutDialog.h>
-#include <QRegisterDialog.h>
-#include <QDatabaseManager.h>
-#include <QCheckInWindow.h>
-#include <QCheckOutWindow.h>
-#include <QParkingSplashscreen.h>
-#include <QAdministratorWindow.h>
+int main(int argc, char *argv[]) {
+  // load the QSS file
+  QFile styleFile(":/qss/style.qss");
+  styleFile.open(QFile::ReadOnly);
 
-#include <QApplication>
-#include <QSplashScreen>
-#include <QDir>
+  QString styleSheet(styleFile.readAll());
+  styleFile.close();
 
-QString admin = "ROOT";
-QString session;
-QString l_name;
+  // create the application
+  QApplication app(argc, argv);
+  app.setStyleSheet(styleSheet);
 
-QString hostname = "localhost";
-QString username = "test";
-QString password = "erick653";
-QString dbname = "POS";
-QString driver = "QMYSQL";
+  // create the QML engine
+  QQmlApplicationEngine engine;
+  engine.load(QUrl(QString("qrc:/qml/main.qml")));
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-
-    QPixmap splash_pixmap(":/images/splash");
-    QSplashScreen splash(splash_pixmap);
-
-    splash.show();
-
-    //ps.show();
-
-    a.processEvents();
-    splash.showMessage("Starting Database connections...", Qt::AlignLeft | Qt::AlignTop);
-//    QString database_name = qApp->applicationDirPath()
-//            + QDir::separator()
-//            + "POS.db";
-    QDatabaseManager db(driver, hostname, username, password, dbname);
-
-    splash.showMessage("Starting application...", Qt::AlignLeft | Qt::AlignTop);
-
-    QLoginDialog l;
-    QRegisterDialog r;
-    QCheckInWindow i;
-    QCheckOutWindow o;
-    QAdministratorWindow adminWindow;
-
-    qDebug() << "initAllTables: " << db.initAllTables();
-
-    splash.showMessage("Initializing database manager...", Qt::AlignLeft | Qt::AlignTop);
-    r.setDatabaseManager(db);
-    l.setDatabaseManager(db);
-    i.setDatabaseManager(db);
-    o.setDatabaseManager(db);
-    adminWindow.setDatabaseManager(db);
-
-    l.setSessionString(session);
-    l.setLoginName(l_name);
-
-    splash.finish(&l);
-    while(!db.userExist(admin))
-    {
-        qDebug() << "User ROOT doesn't exist!";
-        r.exec();
-    }
-    l.exec();
-    qDebug() << "Esta es la salida seleccionada: " << session;
-    if(session == "Caseta")
-    {
-        o.show();
-    }
-    else if(session == "Boletera")
-    {
-        i.show();
-    }
-    else if(session == "Administrador")
-    {
-        adminWindow.show();
-    }
-    return a.exec();
+  // run the application
+  return app.exec();
 }
